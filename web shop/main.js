0e63b3f665b4d3504d89f50aac2a9e0bd0f9f95e
Mario Sutalo <mario.sutalo@gmail.com>
2024-10-10 09:31:19 +0200

    -cp

--- web shop/main.js
+++ web shop/main.js
@@ -2,7 +2,7 @@ const products = [
     {
         id: 1,
         image: 'https://www.instar-informatika.hr/slike/velike/racunalo-instar-gamer-profundis-intel-core-i3-10100f-up-to-4-21154-10100f-pro-ssd500-1650_208307.jpg',
-        name: 'Računalo INSTAR Gamer Profundis, Intel Core i3 10100F',
+        name: 'Računalo i3 10100F',
         details: 'https://www.instar-informatika.hr/racunalo-instar-gamer-profundis-intel-core-i3-10100f-up-to-430ghz-8/99889/product/',
         procesor: 'Intel i3 10100',
         ram: '16gb',
@@ -12,7 +12,7 @@ const products = [
     {
         id: 2,
         image: 'https://www.instar-informatika.hr/slike/male/racunalo-instar-gamer-prime-intel-core-i5-10400f-up-to-43ghz-36794-10400f-pri-ssd500-1650_1.jpg',
-        name: 'Računalo INSTAR Gamer Profundis, Intel Core i5 12000F',
+        name: 'Računalo i5 12000F',
         details: 'https://www.instar-informatika.hr/racunalo-instar-gamer-prime-intel-core-i5-10400f-up-to-43ghz-16gb-d/99887/product/',
         procesor: 'Intel i5 12000',
         ram: '16gb',
@@ -21,12 +21,21 @@ const products = [
     },
 ]
 
-function displayProducts() {
+function displayProducts(filteredProducts) {
     const productsDiv = document.querySelector('.products-div')
-    for (let index = 0; index < products.length; index++) {
-        const product = products[index]
-        const genHtml = generateProductHTML(product)
-        productsDiv.innerHTML += genHtml
+    productsDiv.innerHTML = ""
+    if (filteredProducts) {
+        for (let index = 0; index < filteredProducts.length; index++) {
+            const product = filteredProducts[index]
+            const genHtml = generateProductHTML(product)
+            productsDiv.innerHTML += genHtml
+        }
+    } else {
+        for (let index = 0; index < products.length; index++) {
+            const product = products[index]
+            const genHtml = generateProductHTML(product)
+            productsDiv.innerHTML += genHtml
+        }
     }
 }
 
@@ -41,5 +50,18 @@ function generateProductHTML(product) {
             <button class="add-to-cart-btn">u košaricu</button>
         </div>`
         return generatedHtml
+}
+
+function filterProducts() {
+    const filteredProducts = []
+    const inputElement = document.getElementById('products-input')
+    const searchTerm = inputElement.value
+    for(i = 0; i < products.length; i++) {
+        const product = products[i]
+        if (product.name === searchTerm) {
+            filteredProducts.push(product)
+        }
+    }
+    displayProducts(filteredProducts)
 }
 
