a5d7c8e6ea13c2895567b5245a73d24ad116d43f
Marin <marinsuman69@gmail.com>
2025-03-13 09:33:58 +0100

    halo

--- web shop/app.js
+++ web shop/app.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const mysql = require('mysql2')
+import express from 'express'
+import mysql from 'mysql2/promise'
 
 const app = express()
 
-const db = mysql.createConnection({
+const appConstants = {
+    productsPerPage: 5
+}
+
+// Create the connection to database
+const db = await mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
-    database: 'webshop'
-})
+    database: 'webshop',
+    password: 'password'
+});
 
-db.connect((error) => {
-    if (error) {
-        console.log('Error connecting to db', error)
-        return
-    }
-    console.log('Db Connection succesfull')
-})
-    
 app.set('view engine', 'ejs')
 
 app.listen(3000)
@@ -25,16 +22,59 @@ app.listen(3000)
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    db.query('select * from products', (error, result) => {
-        if (error) {
-            console.log('db error selecting products', error)
-            res.render('index', { title: 'Products', error: error })
-        } else {
-            // console.log('products:', result)
-            getCategoriesFromDb(res, result, 'Products')
+app.get('/', async (req, res) => {
+    const page = parseInt(req.query.page) || 1
+    const productId = req.query.productId || 1;
+    const offset = (page - 1) * appConstants.productsPerPage
+    const getProductsQuery = `select * from products limit ${offset}, ${appConstants.productsPerPage}`
+    const getProductsCountQuery = 'select count(*) as count from products'
+    const getCategoriesQuery = 'select * from categories'
+    try {
+        const [productsResults, fields] = await db.query(getProductsQuery)
+        const [productsCountResults, productsCountFields] = await db.query(getProductsCountQuery)
+        const [categoriesResult, categoriesFields] = await db.query(getCategoriesQuery)
+        const productsCount = productsCountResults[0].count
+        const totalPages = Math.ceil(productsCount / appConstants.productsPerPage)
+        res.render('index', { title: 'Products', products: productsResults, totalPages: totalPages, currentPage: page })
+    } catch (error) {
+        console.log('Error connecting to db', error)
+        res.render('index', { title: 'Products', error: error })
+    }
+})
+
+app.get('/product', async (req, res) => {
+    const productId = req.query.product_id
+    const getProductDetailsQuery = `select products.*, product_images.image_url
+        from products left join product_images
+        on products.id = product_images.product_id
+        where products.id = ${productId};`
+    try {
+        const [productDetailsResult, fields] = await db.query(getProductDetailsQuery)
+        // console.log('Product details:', productDetailsResult)
+        let product = null
+        productDetailsResult.forEach((row) => {
+            if (product === null) {
+                product = {
+                    id: row.id,
+                    name: row.name,
+                    price: row.price,
+                    stock: row.stock,
+                    likes: row.likes,
+                    waranty: row.waranty,
+                    description: row.description,
+                    images: [] 
+                }
+            }
+            product.images.push(row.image_url)
+        })
+        if (product) {
+            console.log(`product with images, ${product.images}`)
         }
-    })
+
+        res.render('product details', { title: product.productName, productDetails: product })
+    } catch (error) {
+        console.log('Error fetching product details', error)
+    }
 })
 
 app.get('/blog', (req, res) => {
@@ -44,14 +84,3 @@ app.get('/blog', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
 })
-
-function getCategoriesFromDb(res, products, title) {
-    db.query('select * from categories', (error, result) => {
-        if (error) {
-            res.render('index', { title: title, error: 'Db Error!!!' })
-        } else {
-            // console.log(`Products: ${products}, categories: ${result ?? []}`)
-            res.render('index', {categories: result, products: products, title: title})
-        }
-    })
-}
