137ace80f94a229d9c16b38a61f6354c5233261d
mario sutalo <mario.sutalo@gmail.com>
2025-03-27 09:18:28 +0100

    -cp

--- web shop/app.js
+++ web shop/app.js
@@ -1,15 +1,19 @@
 import express from 'express'
 import mysql from 'mysql2/promise'
 import productSpecsToArray from './util/util.js'
+import homeRouter from './routes/homeRoutes.js'
+import productRouter from './routes/productRoutes.js'
+import aboutRouter from './routes/aboutRoute.js'
+import blogRouter from './routes/blogRoutes.js'
 
 const app = express()
 
-const appConstants = {
+export const appConstants = {
     productsPerPage: 5
 }
 
 // Create the connection to database
-const db = await mysql.createConnection({
+export const db = await mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'webshop',
@@ -24,81 +28,10 @@ app.listen(3000)
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-    const page = parseInt(req.query.page) || 1
-    const productId = Number.isNaN(parseInt(req.query.productId)) ? 1 : parseInt(req.query.productId)
-    const offset = (page - 1) * appConstants.productsPerPage
-    const getProductsQuery = `select * from products limit ${offset}, ${appConstants.productsPerPage}`
-    const getProductsCountQuery = 'select count(*) as count from products'
-    const getCategoriesQuery = 'select * from categories'
-    try {
-        const [productsResults, fields] = await db.query(getProductsQuery)
-        const [productsCountResults, productsCountFields] = await db.query(getProductsCountQuery)
-        const [categoriesResult, categoriesFields] = await db.query(getCategoriesQuery)
-        const productsCount = productsCountResults[0].count
-        const totalPages = Math.ceil(productsCount / appConstants.productsPerPage)
-        res.render('index', { title: 'Products', products: productsResults, totalPages: totalPages, currentPage: page })
-    } catch (error) {
-        console.log('Error connecting to db', error)
-        res.render('index', { title: 'Products', error: error })
-    }
-})
+app.use('/', homeRouter)
 
-app.get('/product', async (req, res) => {
-    // sql injection attack example
-    //http://localhost:3000/product?product_id=1; delete from product_images; - try it :)
-    // SELECT * from products where id = 1; delete from product_images;
-    const productId = req.query.product_id
+app.use('/product', productRouter)
 
-    // variable interpolation - unsafe
-    const getProductDetailsQueryDangeroues = `select products.*, product_images.image_url
-        from products left join product_images
-        on products.id = product_images.product_id
-        where products.id = ${productId};`
+app.use('/blog', blogRouter)
 
-    // parametrized query - safe
-    const getProductDetailsQueryWithPlaceholdersSafe = `select products.*, product_images.image_url
-        from products left join product_images
-        on products.id = product_images.product_id
-        where products.id = ?;`
-
-    try {
-        // const [productDetailsResult, fields] = await db.query(getProductDetailsQuery)
-        const [productDetailsResult, fields] = await db.query(getProductDetailsQueryWithPlaceholdersSafe, [productId])
-        let product = null
-        productDetailsResult.forEach((row) => {
-            if (product === null) {
-                product = {
-                    id: row.id,
-                    name: row.name,
-                    specs: productSpecsToArray(row.specs),
-                    price: row.price,
-                    image: row.image_url,
-                    stock: row.stock,
-                    likes: row.likes,
-                    warranty: row.warranty,
-                    description: row.description,
-                    images: []
-                }
-            }
-            product.images.push(row.image_url)
-        })
-        if (product !== null) {
-            console.log(`product with images:`, product)
-            res.render('product details', { title: product.productName, productDetails: product })
-        } else {
-            // to do implement product doesn't exist page
-        }
-
-    } catch (error) {
-        console.log('Error fetching product details', error)
-    }
-})
-
-app.get('/blog', (req, res) => {
-    res.render('blog', { title: 'Blog' })
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' })
-})
+app.use('/about', aboutRouter)
---
+++ web shop/routes/productRoutes.js
@@ -0,0 +1,58 @@
+import express from 'express'
+import { db } from '../app.js'
+import productSpecsToArray from '../util/util.js'
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    // sql injection attack example
+    //http://localhost:3000/product?product_id=1; delete from product_images; - try it :)
+    // SELECT * from products where id = 1; delete from product_images;
+    const productId = req.query.product_id
+
+    // variable interpolation - unsafe
+    const getProductDetailsQueryDangeroues = `select products.*, product_images.image_url
+        from products left join product_images
+        on products.id = product_images.product_id
+        where products.id = ${productId};`
+
+    // parametrized query - safe
+    const getProductDetailsQueryWithPlaceholdersSafe = `select products.*, product_images.image_url
+        from products left join product_images
+        on products.id = product_images.product_id
+        where products.id = ?;`
+
+    try {
+        // const [productDetailsResult, fields] = await db.query(getProductDetailsQuery)
+        const [productDetailsResult, fields] = await db.query(getProductDetailsQueryWithPlaceholdersSafe, [productId])
+        let product = null
+        productDetailsResult.forEach((row) => {
+            if (product === null) {
+                product = {
+                    id: row.id,
+                    name: row.name,
+                    specs: productSpecsToArray(row.specs),
+                    price: row.price,
+                    image: row.image_url,
+                    stock: row.stock,
+                    likes: row.likes,
+                    warranty: row.warranty,
+                    description: row.description,
+                    images: []
+                }
+            }
+            product.images.push(row.image_url)
+        })
+        if (product !== null) {
+            console.log(`product with images:`, product)
+            res.render('product details', { title: product.productName, productDetails: product })
+        } else {
+            // to do implement product doesn't exist page
+        }
+
+    } catch (error) {
+        console.log('Error fetching product details', error)
+    }
+})
+
+export default router
