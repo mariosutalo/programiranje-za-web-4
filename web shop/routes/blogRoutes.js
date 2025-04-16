2b8146e891804c23412f6465528d149a8f9e0bf0
mario sutalo <mario.sutalo@gmail.com>
2025-04-16 11:38:19 +0200

    -cp

--- web shop/routes/blogRoutes.js
+++ web shop/routes/blogRoutes.js
@@ -3,7 +3,11 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res)=> {
-    res.render('blog', { title: 'Blog' })
+    res.render('blogs', { title: 'Blog' })
+})
+
+router.get('/read', (req, res) => {
+    res.render('singleBlog', { title: 'Single blog'})
 })
 
 export default router
