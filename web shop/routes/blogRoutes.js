216bed34e44cdadc53068f9f69bc9a1fd7f1a489
Mario Šutalo <mario.sutalo@gmail.com>
2025-05-13 15:30:15 +0200

    -cp

--- web shop/routes/blogRoutes.js
+++ web shop/routes/blogRoutes.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import { db } from '../app.js'
 
 const router = express.Router()
 
-router.get('/', (req, res)=> {
-    res.render('blogs', { title: 'Blog' })
+router.get('/', async (req, res) => {
+    const selectAllBlogsQuery = 'select * from blogs'
+    const [results, fields] = await db.execute(selectAllBlogsQuery)
+    res.render('blogs', { title: 'Blog', blogs: results })
 })
 
-router.get('/read', (req, res) => {
-    res.render('singleBlog', { title: 'Single blog'})
+router.get('/readBlog', async (req, res) => {
+    const blogId = req.query.blogId
+    const selectSingleBlogQuery = 'select * from blogs where id = ?'
+    const [results, fields] = await db.execute(selectSingleBlogQuery, [blogId.toString()])
+    console.log('Query result', results)
+    res.render('singleBlog', { title: `${blogId}`, blog: results[0] })
 })
 
 export default router
