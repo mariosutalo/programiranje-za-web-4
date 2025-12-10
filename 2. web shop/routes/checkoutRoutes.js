f737d99cb8ee492dd49be00a024a9d4e829506cb
Mario Šutalo <mario.sutalo@gmail.com>
2025-12-10 09:41:32 +0100

    -cp

--- 2. web shop/routes/checkoutRoutes.js
+++ 2. web shop/routes/checkoutRoutes.js
@@ -1,14 +1,41 @@
 import express from 'express'
+import { db } from '../app.js'
+
 
 const router = express.Router()
 
-router.get('/', (req, res)=> {
+router.get('/', (req, res) => {
     res.render('checkout', { title: 'Checkout' })
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const shippingData = req.body
-    console.log(shippingData)
+    console.log('form data', shippingData)
+    const getCartProductsQuery = `
+    SELECT c.product_id as productId, c.quantity, p.price
+    from cart_items as c
+    join products p on c.product_id = p.id;`
+    const cartItemsResponse = await db.execute(getCartProductsQuery)
+    //console.log('Cart items:', cartItemsResponse[0])
+    //await db.beginTransaction()
+    try {
+        const createOrderSql = `insert into orders(full_name, street, zip_code, city, phone, session_guid)
+                            values (?,?,?,?,?,?)`
+        const insertOrderResult = await db.execute(createOrderSql, [
+            shippingData.fullname,
+            shippingData.street,
+            shippingData.zipcode,
+            shippingData.city,
+            shippingData.phone,
+            req.cookies.sessionId
+        ])
+    } catch (error) {
+        await db.rollback()
+        console.log('error creating order', error)
+    }
+
+
+
     res.redirect(req.get('Referrer') || '/')
 })
 
