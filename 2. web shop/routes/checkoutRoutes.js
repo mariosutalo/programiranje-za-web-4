d6fb66d2cf7c135a9865b7f4b3892f568255c616
Mario Šutalo <mario.sutalo@gmail.com>
2026-01-28 09:12:43 +0100

    -cp

--- 2. web shop/routes/checkoutRoutes.js
+++ 2. web shop/routes/checkoutRoutes.js
@@ -18,6 +18,7 @@ router.post('/', async (req, res) => {
     where session_guid = ?;`
     const cartItemsResponse = await db.execute(getCartProductsQuery, [req.cookies.sessionId])
     console.log('Cart items:', cartItemsResponse[0])
+    const cartItems = cartItemsResponse[0]
     await db.beginTransaction()
     try {
         const createOrderSql = `insert into orders(full_name, street, zip_code, city, phone, session_guid)
@@ -30,17 +31,20 @@ router.post('/', async (req, res) => {
             shippingData.phone,
             req.cookies.sessionId
         ])
-        //collect values
-        //const insertOrderitemsSql = 'insert into order_items(product_id, quantity, price, order_id) values ?'
-        //res.redirect(`/payment`)
-        //await db.query(query, [values])
-        //const values = cartItems.map(i => [orderId, i.productId, i.qty, i.price]);
+        const orderId = insertOrderResult[0].insertId
+        const insertOrderItemsQuery = 'insert into order_items(product_id, quantity, price, order_id) values ?'
+        const orderItemsToInsert = cartItems.map(cartItem => {
+            return [cartItem.productId, cartItem.quantity, cartItem.price, orderId]
+        })
+        const insertOrderItemsResult = await db.query(insertOrderItemsQuery, [orderItemsToInsert])
+        throw new Error('testing db rollback')
         await db.commit()
+        res.redirect('/payment')
     } catch (error) {
         await db.rollback()
         console.log('error creating order', error)
+        res.redirect(req.get('Referrer') || '/')
     }
-    res.redirect(req.get('Referrer') || '/')
 })
 
 export default router
